6bafa89c5e35d9f496ba09a9584708bacac2e2aa
Thales <teste19641@outlook.com>
2025-06-05 11:16:18 -0300

    dia 6

--- Poject/Interface/Interface_main.py
+++ Poject/Interface/Interface_main.py
@@ -1,7 +1,14 @@
 import sys
 
 from PySide6.QtWidgets import QApplication, QPushButton
-,
+
 app = QApplication(sys.argv)
 
-botao = 
+botao = QPushButton("Texto botao 1")
+botao.setStyleSheet("font-size: 40px; color: red")
+
+botao2 = QPushButton("Texto botao 2")
+
+
+botao.show()
+app.exec()
